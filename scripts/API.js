e75aec62d82dc52be5f1001b85e9c7e0fae45e46
AxelAndersson <axelgandersson@gmail.com>
2025-04-17 15:05:21 +0200

    kommenterade ut imorport env fil i api.js så att koden jag köras

--- scripts/API.js
+++ scripts/API.js
@@ -1,38 +1,38 @@
-import { UNSPLASH_API_KEY } from '../env.js'
+// import { UNSPLASH_API_KEY } from '../env.js'
 
 //Getting users from jsonplaceholder
-export async function getUsers () {
-    // Url to fetch
-    const url = "https://jsonplaceholder.typicode.com/users";
+export async function getUsers() {
+	// Url to fetch
+	const url = "https://jsonplaceholder.typicode.com/users";
 
-    try {
-        const response = await fetch(url);
-        //If response is not ok throw error
-        if(!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        //Return json object
-        return json;
-    } catch(error) {
-        console.error(error.message);
-    }
+	try {
+		const response = await fetch(url);
+		//If response is not ok throw error
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+		const json = await response.json();
+		//Return json object
+		return json;
+	} catch (error) {
+		console.error(error.message);
+	}
 }
 
 // Getting images from unsplash
-export async function getImages(query = 'face') {
-    // API_KEY from env.js (.gitignore´d)
-    const accessKey = UNSPLASH_API_KEY;
-    const url = `https://api.unsplash.com/photos/random?query=${query}&client_id=${accessKey}`;
+export async function getImages(query = "face") {
+	// API_KEY from env.js (.gitignore´d)
+	const accessKey = UNSPLASH_API_KEY;
+	const url = `https://api.unsplash.com/photos/random?query=${query}&client_id=${accessKey}`;
 
-    try {
-        const response = await fetch(url);
-        if(!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        return json;
-    } catch(error) {
-        console.error(error.message);
-    }
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+		const json = await response.json();
+		return json;
+	} catch (error) {
+		console.error(error.message);
+	}
 }
