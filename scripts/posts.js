ff3dc7e2dd820a79a6bf553d589d6467628fd3af
nadja-mm <nadja@molarin.se>
2025-04-27 22:40:48 +0200

    utvecklade posts js och la till enkel css

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,3 +1,4 @@
+//fetch all posts from API
 async function getpost() {
 	const url = "https://jsonplaceholder.typicode.com/posts";
 
@@ -15,20 +16,31 @@ async function getpost() {
 	}
 }
 
-
 function renderPosts() {
-    const container = document.getElementById("posts-section")
-    const userId = Number(localStorage.getItem('id'))
-    console.log(typeof userId)
-    console.log(container)
-    const posts = getpost().then((posts) => {
-        const results = posts.filter((post) => post.userId === userId);
-        console.log(results)
+    const container = document.getElementById("posts-section");
+    const userId = Number(localStorage.getItem('id'));
+
+    //if container already has posts, clear and return
+    if(container.children.length > 0) {
+        container.innerHTML = "";
+        return;
+    }
+    //or fetch and render posts
+    getpost()
+        .then(posts => {
+        //filter for users posts (max 10)
+        const results = posts
+        .filter(post => post.userId === userId)
+        .slice(0, 10);
+
+        //rendera only 10 posts
         results.forEach(post => {
             const block = document.createElement("div")
             block.innerHTML = `<div class="posts-div">${post.title}</div>`
             container.appendChild(block)
         });
+    })
+    .catch(err => {
+        console.error("Fel vid rendering");
     });
-    console.log(posts)
-};
+}
