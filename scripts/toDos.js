7d4b636dba5ca61c0916a098e495a954ad7eb3bc
RNGDiff <makwan_@hotmail.com>
2025-04-28 14:05:16 +0200

    Fixed Padding - Cleared containers - Fixed Font

--- scripts/toDos.js
+++ scripts/toDos.js
@@ -12,7 +12,9 @@ fetch('https://jsonplaceholder.typicode.com/todos')
 
 const toDosContainer = document.getElementById('todo-container');
 const toDoElement = document.getElementById('to-do-btn');
+const postsElement = document.getElementById('posts-section');
 toDoElement.addEventListener('click',()=>{
+    postsElement.innerHTML = '';
     toDosContainer.innerHTML = '';
     toDoArray.forEach(toDosData => {
         if(toDosData.userId == Number(usersId)){
@@ -20,7 +22,7 @@ toDoElement.addEventListener('click',()=>{
                 console.log(usersId);
                 console.log(toDosData);
                 const content = document.createElement('div');
-                const title = document.createElement('h1');
+                const title = document.createElement('p');
                 const completed = document.createElement('p');
                 content.id = 'todo-content';
                 title.id = 'todo-title';
@@ -40,55 +42,5 @@ toDoElement.addEventListener('click',()=>{
     });
 
 
-
-
-
-
-
-
-
-
-
-// import { getUsers } from "./API.js";
-// let toDoArray = [];
-
-// fetch('https://jsonplaceholder.typicode.com/todos')
-// .then(response => response.json())
-// .then(data =>{ toDoArray = data})
-// .catch(error => console.error('Error', error));
-
-// const toDosContainer = document.createElement('div');
-// toDosContainer.classList.add('todo-container');
-// const profileName = document.querySelector('.profile-name');
-// const toDoElement = document.querySelector('.to-do-btn');
-
-// getUsers().then(users =>{
-//     console.log(users);
-//     console.log(users.id);
-// })
-
-// toDoArray.forEach(toDosData => {
-//     if(toDosData.userId == users.id){
-//         console.log(toDosData.title);
-//         toDosContainer.innerHTML = '';
-//         toDoElement.addEventListener('click',()=>{
-//             const content = document.createElement('div');
-//             const title = document.createElement('h1');
-//             const completed = document.createElement('p');
-//             title.classList.add('title');
-//             completed.classList.add('completed');
-//             title.textContent = toDosData.title;
-//             if(toDosData.completed == false){
-//                 completed.textContent = '❌';
-//             }
-//             else{
-//                 completed.textContent = '✅'
-//             }
-//             toDosContainer.appendChild(content);
-//             content.appendChild(title);
-//             content.appendChild(completed);
-//         })
-//     };
-// });
 
 
